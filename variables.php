53469d027fd060aa324ebe900b8652b935097066
william alvarez <waalvarez1@misena.edu.co>
2023-06-16 14:38:23 -0500

    variables php

---
+++ variables.php
@@ -0,0 +1,22 @@
+<?php
+
+// variables y nombres en php 
+
+$nombre ="william arvey";
+
+// es posible en php iniciar con guiòn bajo
+
+$_apellido="alvarez bolaños";
+
+/* no esta permitido iniciar con caracteres 
+especiales   @&%$·"*/ 
+
+// la variables en php son sencibles a mayusculas
+
+$color = "morao";
+$Color="Azul";
+$COLOR="Verde";
+
+echo $COLOR;
+
+?>
