ff83382f5bd9322c2d5a258663949c3e01765ad8
william alvarez <waalvarez1@misena.edu.co>
2023-06-20 01:01:03 -0500

    ensayo live server

---
+++ operadoresAritmeticos.php
@@ -0,0 +1,38 @@
+<?php
+
+/* 
+
+=============================================
+operadores aritmeticos 
+
+suma + suma de a + b
+resta - diferencia a - b
+multiplicacion * producto a * b
+division /, cociente de a / b
+modulo %, resto de a dividido b 
+==============================================
+
+*/
+
+$a = 10;
+$b = 20;
+$suma = $a + $b;
+echo $suma;
+echo $suma;
+echo $suma;
+echo $suma;
+echo $suma;
+echo $suma;
+echo $suma;
+
+$resta= $a - $b;
+echo $resta;
+echo $suma;
+
+
+
+
+
+
+
+?>
