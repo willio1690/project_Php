2ce6cc03086ed2a648b9673c69ed7456692857a4
william alvarez <waalvarez1@misena.edu.co>
2023-06-20 02:39:58 -0500

    salto de linea <br>

--- operadoresAritmeticos.php
+++ operadoresAritmeticos.php
@@ -14,20 +14,25 @@ modulo %, resto de a dividido b
 
 */
 
-$a = 10;
+$a = 10;   
 $b = 20;
 $suma = $a + $b;
-echo $suma;
-echo $suma;
-echo $suma;
-echo $suma;
-echo $suma;
-echo $suma;
-echo $suma;
+//la etiqueta \n salto de linea no funciona es <br>
+echo $suma ."<br>". "resta\t" ;
+echo $suma ."<br>". "resta";
 
-$resta= $a - $b;
-echo $resta;
-echo $suma;
+
+
+
+
+
+/*
+=========================================
+operadores de asignacion
+
+ = += *= /= .=
+
+*/
 
 
 
