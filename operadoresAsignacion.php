d4450e3d4c41f6874760c6d363872c74a21bf861
william alvarez <waalvarez1@misena.edu.co>
2023-06-20 11:50:18 -0500

    operadores de asignacion combiandos

---
+++ operadoresAsignacion.php
@@ -0,0 +1,45 @@
+
+<?php
+/*
+=========================================
+operadores de asignacion
+=
+
+operadores de asignacion combinados
+
+ = += *= /= .=
+
+*/
+
+$a=5;
+$b=10;
+
+$a+=$b;
+echo $a."<br>";
+
+/*
+  ============================================
+  se asignan los valores a = 5 y b = 10 
+  en la linea 17 suma a + b 
+  luego el resultado se asigna a la variable a 
+  =============================================
+  
+*/
+
+$a-=$b;
+echo $a."<br>";
+
+/*
+===============================================
+operadores de asignacion concatenacion 
+
+=.
+
+*/
+
+$a.=$b;
+echo $a;
+
+
+
+?>
