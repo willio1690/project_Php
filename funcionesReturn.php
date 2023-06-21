fdd2aa1107cd3efa41bb5ed610e2ebdca614ff1e
william alvarez <waalvarez1@misena.edu.co>
2023-06-20 23:56:23 -0500

    funcion return

---
+++ funcionesReturn.php
@@ -0,0 +1,36 @@
+<?php
+function sumarNumeros($num1,$num2,$num3){
+
+    /*
+    ========================================
+    return 
+
+    retorna los valores de la funcion pero
+    no los imprime cuando llamo la funcion
+    retorna los valores que le estoy pasando
+    
+    ========================================
+    */
+    return $num1+$num2+$num3;
+
+
+}
+
+/*
+=============================================
+$sumaTotal
+
+Como return devuelve los valores asignados 
+cuando utilizo la funcion, entonces debo
+asignar lo que retorna de informacion a 
+una variable en este caso sumaTotal
+============================================
+
+*/
+
+$sumaTotal=sumarNumeros(1,24,19);
+// se imprime la variable sumaTotal 
+echo "la sumatoria total es:.$sumaTotal";
+
+
+?>
