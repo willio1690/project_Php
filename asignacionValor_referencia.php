2008434b74bb75d741d7669477aab06ecc17c924
william alvarez <waalvarez1@misena.edu.co>
2023-08-12 21:51:16 -0500

    examples of values for references and values

--- asignacionValor_referencia.php
+++ asignacionValor_referencia.php
@@ -18,8 +18,9 @@ no afecta al valor original fuera de ella.
 ==========================================
 */
 // Definición de la función para multiplicar un número por 2
-$numero1=2;
-function duplicar($numero1) {
+$numero1 = 2;
+function duplicar($numero1)
+{
     $numero1 *= 2; // Multiplicamos el número por 2
     return $numero1; // Devolvemos el resultado
 }
@@ -30,7 +31,7 @@ $numero_original = 3;
 $numero_duplicado = duplicar($numero_original);
 
 // Mostrar resultados
-echo "numero:".$numero1."<br>";
+
 echo "Número original: " . $numero_original . "<br>";
 echo "Número duplicado: " . $numero_duplicado . "<br>";
 
@@ -43,18 +44,87 @@ Ejemplo
 
 
 
-$numerop=16;
-function dividir($numerop){
-    $numerop /=3;
+//$numerop=16;
+function dividir($numerop)
+{
+    $numerop /= 3;
     return $numerop;
-
 }
 
-$numero_div =13;
+$numero_div = 13;
 $numero_divi = dividir($numero_div);
 
 // Mostrar resultados division
 echo "Número dividido: " . $numero_divi . "<br>";
+
+
+
+/* 
+
+=========****===*****======*****===*****=====*****=====*****
+
+ejercicios con funciones por valor 
+
+*/
+
+function hacer_cafe($tipo = "capuchinno")
+{
+    // funcion hacer cafe y recibe un parametro llamado capucchino
+
+    return "el cafe es de $tipo <br>";
+    /*
+una funcion para ser utilizada en el contexto debe tener
+siempre un return para poder utilizarla
+
+en este caso retornamos una cadena de estring concatenado
+con la variable tipo acompañado con un salto de linea 
+
+*/
+}
+
+echo hacer_cafe();
+/*
+para utilizar la funcion fuera de su contexto es 
+necesario utilizar el nombre seguido de los 
+parentesis aqui toma el valor por defecto
+*/
+
+echo hacer_cafe(null);
+
+/*
+en este caso tambien me respeta el parametro
+que debo pasarle 
+
+*/
+echo hacer_cafe("moka<br>");
+/*
+en este caso estamos pasando parametros 
+que permiten utilizar el espacio 
+sin remplazar el valor original 
+
+*/
+
+/*
+es posible pasarle varios parametros predefinidos
+en la funcion igualmente por valor 
+
+
+*/
+
+function persona($sunombre = "paula", $suApellido = "garcia", $edad = 16)
+{
+    return "Bienvenida $sunombre $suApellido de $edad años de edad";
+}
+//si no pasamos los valores completos sufre cambios inesperados
+echo persona() . "<br>";
+echo persona("paulita");
+
+/*
+si utilizamos solo una variable solo cambia esa dado que
+si remplaza el valor pero deja intacta la variable original 
+
+*/
+
 
 
 /*
@@ -74,14 +144,15 @@ la variable.
 // Función que incrementa el valor de una variable por referencia
 
 
-function incrementar(&$numero) {
+function incrementar(&$numero)
+{
     $numero++; // Incrementa el valor de $numero en 1
 }
 
 // Variable inicial
 $miNumero = 5;
 
-echo "Antes de incrementar: $miNumero <br>";
+echo "<br>Antes de incrementar: $miNumero <br>";
 
 // Llamada a la función
 incrementar($miNumero); // Se pasa $miNumero como referencia
@@ -100,12 +171,14 @@ ejemplo
 $miNumero = 6;
 
 // Función que verifica si un número es par
-function esPar($numero) {
+function esPar($numero)
+{
     return $numero % 2 == 0;
 }
 
 // Función que duplica un número por referencia
-function duplicar1(&$numero) {
+function duplicar1(&$numero)
+{
     $numero *= 2;
 }
 
@@ -115,7 +188,7 @@ function duplicar1(&$numero) {
 if (esPar($miNumero) && duplicar1($miNumero)) {
     echo "El número es par y se ha duplicado: $miNumero";
 } else {
-    echo "El número no es par o no se ha podido duplicar";
+    echo "El número no es par o no se ha podido duplicar<br>";
 }
 
 /*
@@ -134,53 +207,22 @@ condiciones son verdaderas, se imprime el mensaje
 */
 
 
+/*
+ 
+ejemplo asignacion por referencia 
 
 
+ */
 
+function referencia(&$parametro)
+{
 
+    //
 
+    $parametro = 'cambio de valor';
+}
+$elNombre = 'codigofacilito';
+print "{$elNombre}<br>";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+referencia($elNombre);
+print "{$elNombre}\n"; 
